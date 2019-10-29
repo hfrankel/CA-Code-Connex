99ed1e00103cf6a9f7807ef704aaa89774c69c24
Harry Frankel <hfranks92@gmail.com>
2019-10-29 16:09:17 +1100

    Added ratings, sessions, technologies, technologies_tutors and tutors tables. Added firstname and lastname columns to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_040320) do
+ActiveRecord::Schema.define(version: 2019_10_29_050136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,60 @@ ActiveRecord::Schema.define(version: 2019_10_29_040320) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "ratings", force: :cascade do |t|
+    t.integer "score"
+    t.text "comment"
+    t.bigint "session_id"
+    t.bigint "tutor_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_ratings_on_session_id"
+    t.index ["tutor_id"], name: "index_ratings_on_tutor_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "timestamp"
+    t.integer "duration"
+    t.integer "cost"
+    t.string "stripe"
+    t.text "note"
+    t.bigint "tutor_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tutor_id"], name: "index_sessions_on_tutor_id"
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "technologies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "technologies_tutors", force: :cascade do |t|
+    t.bigint "tutor_id"
+    t.bigint "technology_id"
+    t.integer "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["technology_id"], name: "index_technologies_tutors_on_technology_id"
+    t.index ["tutor_id"], name: "index_technologies_tutors_on_tutor_id"
+  end
+
+  create_table "tutors", force: :cascade do |t|
+    t.integer "pricing"
+    t.integer "experience"
+    t.text "bio"
+    t.text "style"
+    t.boolean "active"
+    t.integer "maxduration"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tutors_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -44,9 +98,18 @@ ActiveRecord::Schema.define(version: 2019_10_29_040320) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "firstname"
+    t.string "lastname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "ratings", "sessions"
+  add_foreign_key "ratings", "tutors"
+  add_foreign_key "sessions", "tutors"
+  add_foreign_key "sessions", "users"
+  add_foreign_key "technologies_tutors", "technologies"
+  add_foreign_key "technologies_tutors", "tutors"
+  add_foreign_key "tutors", "users"
 end
