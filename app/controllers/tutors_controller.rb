8b847d091bffee307c28ae7c481dd38992ec5b1f
Harry Frankel <hfranks92@gmail.com>
2019-10-31 16:11:10 +1100

    Added instance variables for index and show actions

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -1,11 +1,18 @@
 class TutorsController < ApplicationController
   
   def index
-
+    @tutors = Tutor.includes(:user).all
   end
 
   def show
+    @tutor = Tutor.includes(:technologies_tutors, :technologies).find(params[:id])
+    scores = Tutor.find(params[:id]).ratings.pluck(:score)
+    @ratings_avg_score = scores.sum / scores.count.to_f
+    @sessions_count = Tutor.find(params[:id]).sessions.pluck(:id).count
 
+    @master_tech = @tutor.technologies_tutors.select { |i| i.level == "master" }
+    @experienced_tech = @tutor.technologies_tutors.select { |i| i.level == "experienced" }
+    @fresh_tech = @tutor.technologies_tutors.select { |i| i.level == "fresh" }
   end
 
 end
