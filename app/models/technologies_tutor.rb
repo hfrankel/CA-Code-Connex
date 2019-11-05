bf29ff484c51302b31d3462f580303f1d6541771
Harry Frankel <hfranks92@gmail.com>
2019-11-05 20:17:10 +1100

    Removed space

--- app/models/technologies_tutor.rb
+++ app/models/technologies_tutor.rb
@@ -2,5 +2,5 @@ class TechnologiesTutor < ApplicationRecord
   belongs_to :tutor
   belongs_to :technology
 
-  enum level: [ :fresh, :experienced, :master]
+  enum level: [:fresh, :experienced, :master]
 end
