915851f5bd41c580407b71da1f33ff6058a3cdd3
Harry Frankel <hfranks92@gmail.com>
2019-11-05 20:14:44 +1100

    Added variables to show account profile and edit profile

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,11 +1,22 @@
 class AccountsController < ApplicationController
 
   def show
-    @user = User.find(current_user.id)
+    if current_user == nil
+      redirect_to new_user_session_path
+    else
+      @user = User.find(current_user.id)
+      @tutor = Tutor.find_by(user_id: current_user.id)
+      @active_tutor = Tutor.where(user_id: "#{current_user.id}")
+    end
   end
 
   def update
 
+  end
+
+  def edit
+    @user = current_user
+    @tutor = Tutor.find_by(user_id: current_user.id)
   end
 
   def delete
