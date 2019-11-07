a63293e0e5ef1a3a354ecd4a67a150f4014630ed
Harry Frankel <hfranks92@gmail.com>
2019-11-08 10:43:10 +1100

    Added destroy dependency association to user and tech_tutor models

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,8 +1,8 @@
 class Tutor < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
   has_many :sessions
   has_many :ratings
-  has_many :technologies_tutors
+  has_many :technologies_tutors, dependent: :destroy
   has_many :technologies, through: :technologies_tutors
 
   enum experience: { junior: 0, mid: 1, senior: 2 }
