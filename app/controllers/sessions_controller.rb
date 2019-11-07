719c1fda0b90380966102e22499327715eccf898
Harry Frankel <hfranks92@gmail.com>
2019-11-07 20:05:23 +1100

    Added stripe functionality to new plus added show, success and webhook actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,36 @@
 class SessionsController < ApplicationController
-
 before_action :authenticate_user!
+skip_before_action :verif y_authenticity_token, only: [:webhook]
 
 
   def new
     @tutor_id = params[:tutor_id]
+    @tutor = Tutor.find(@tutor_id)
     @session = Session.includes(:tutor_id).new
     # @tutor = Tutor.includes(:user).find(params[:id])
     # scores = Tutor.find(params[:id]).ratings.pluck(:score)
     # @ratings_avg_score = scores.sum / scores.count.to_f
     # @sessions_count = Tutor.find(params[:id]).sessions.pluck(:id).count
+
+    session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      line_items: [{
+        name: "Tutoring Session",
+        description: "Here is a session",
+        amount: @tutor.pricing,
+        currency: 'aud',
+        quantity: 1,
+      }],
+      payment_intent_data: {
+        metadata: {
+          user_id: current_user.id
+        }
+      },
+      success_url: root_url + "sessions/success",
+      cancel_url: root_url,
+    )
+
+    @session_id = session.id
   end
 
   def create
@@ -22,6 +43,19 @@ before_action :authenticate_user!
     else
         redirect_to root_path
     end
+  end
+
+
+  def show
+
+  end
+
+  def success
+
+  end
+
+  def webhook
+    puts params
   end
 
 
