52d8dae030aa91f9767d0acafd4a5ad14e146eec
Harry Frankel <hfranks92@gmail.com>
2019-11-07 20:07:52 +1100

    Added tutor association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :sessions
+  has_one :tutor
   has_one_attached :pic
 end
