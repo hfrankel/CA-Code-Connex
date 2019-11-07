5e7cdf95aecc530c5144e59aa8ba856b5d330316
Harry Frankel <hfranks92@gmail.com>
2019-11-08 10:44:04 +1100

    Added destroy dependency assocation to tutor

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :sessions
-  has_one :tutor
+  has_one :tutor, dependent: :destroy
   has_one_attached :pic
 end
